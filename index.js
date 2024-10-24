eeb019aa022b8305d965df550d1928c3e3e65d94
moise7593 <moise.fernandez@estiam.com>
2024-10-24 11:08:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/plain');
-res.end('Projet-Azure Fernandez\n');
+res.end('Projet-Azure-Fernandez\n');
 });
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
