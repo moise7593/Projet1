e9be5da5002b4e4a5e590f30a2e72a8b90590027
moise7593 <moise.fernandez@estiam.com>
2024-10-24 18:25:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajouter un Nom</title>
+    <title>Projet</title>
     <script>
         async function ajouterNom() {
             const nom = document.getElementById('nom').value;
